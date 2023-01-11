9e386d32bed1c8cb52ba9bcee54065108a810076
DAKSHAY111 <86140759+DAKSHAY111@users.noreply.github.com>
2023-01-11 20:43:09 +0530

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import NavbarHeader from "./Components/NavbarHeader";
 import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import HomePage from "./Pages/HomePage";
 
-const App = () => {
+// import { AppContext } from "./Context/AppContext";
+
+import "./App.css";
+
+function App() {
   return (
-    <>
-      <NavbarHeader></NavbarHeader>
-    </>
+    // <AppContext.Provider value={{}}>
+    <BrowserRouter>
+      <NavbarHeader />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    // </AppContext.Provider>
   );
-};
+}
 
 export default App;
