a86714877725f4c4dadaa8dd2b61aeb5216d127c
DAKSHAY111 <86140759+DAKSHAY111@users.noreply.github.com>
2023-01-22 12:14:01 +0530

    updated

---
+++ client/src/Services/AppApi.js
@@ -0,0 +1,58 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const appApi = createApi({
+  reducerPath: "appApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/",
+  }),
+  endpoints: (builder) => ({
+    signUpUser: builder.mutation({
+      query: (user) => ({
+        url: "/user/signup",
+        method: "POST",
+        body: user,
+      }),
+    }),
+
+    loginUser: builder.mutation({
+      query: (data) => ({
+        url: "/user/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    logout: builder.mutation({
+      query: (data) => ({
+        url: "/user/logout",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    requestNewPassword: builder.mutation({
+      query: (data) => ({
+        url: "/user/new/password",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    resetPassword: builder.mutation({
+      query: (data) => ({
+        url: "/user/reset/password",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useSignUpUserMutation,
+  useLoginUserMutation,
+  useLogoutMutation,
+  useRequestNewPasswordMutation,
+  useResetPasswordMutation,
+} = appApi;
+export default appApi;
